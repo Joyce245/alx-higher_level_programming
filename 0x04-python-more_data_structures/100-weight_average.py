0f41ba610bb7f975a340efcf423e6500d941b2dc
Talabi, Oluwasefunmi Joy <oluwaseyifunmmyjoyce1@gmail.com>
2022-07-07 01:08:09 +0100

    New update

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if len(my_list) == 0:
-        return 0
-    return sum([x*y for (x, y) in my_list]) / sum([y for (x, y) in my_list])#!/usr/bin/python3
-def weight_average(my_list=[]):
-        if len(my_list) == 0:
-                    return 0
-                    return sum([x*y for (x, y) in my_list]) / sum([y for (x, y) in my_list])
+    if my_list and len(my_list):
+        num = 0
+        dem = 0
+        for tup in my_list:
+            num += (tup[0] * tup[1])
+            dem += tup[1]
+        return (num / dem)
+    return 0

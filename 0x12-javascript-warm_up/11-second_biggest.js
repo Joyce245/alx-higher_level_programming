90d5593bbf2a471f92ca969c2cfec5bc1cd3f920
Talabi, Oluwasefunmi Joy <oluwaseyifunmmyjoyce1@gmail.com>
2022-10-07 04:51:50 +0100

    New update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
-let args = process.argv.slice(2).map((x) => {
-  return parseInt(x);
-});
-
-if (args.length <= 1) {
-  console.log(0);
+if (process.argv[2] && process.argv[3]) {
+  const array = process.argv.slice(2, process.argv.length);
+  const maxn = Math.max.apply(null, array); // get the max of the array
+  const max = maxn.toString(10);
+  const maxi = array.indexOf(max);
+  array[maxi] = -Infinity; // replace max in the array with -infinity
+  const secondMax = Math.max.apply(null, array); // get the new max
+  array[maxi] = max;
+  console.log(secondMax);
 } else {
-  console.log(args.sort((a, b) => {
-    return b - a;
-  })[1]);
+  console.log('0');
 }

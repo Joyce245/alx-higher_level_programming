75cc27896ea31fb87d3bb5a41a0de8108f8598b3
Talabi, Oluwasefunmi Joy <oluwaseyifunmmyjoyce1@gmail.com>
2022-10-10 04:09:49 +0100

    New update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-"""Query name"""
+"""
+takes in an argument and displays all values in the states table
+"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=sys.argv[2],
-                         passwd=sys.argv[1],
-                         db=sys.argv[3])
-
-    cur = db.cursor()
-
-    # Execute the query
-    query = "SELECT id, name FROM states WHERE name='{}'\
-    COLLATE latin1_general_cs ORDER BY states.id ASC;".format(sys.argv[4])
-    cur.execute(query)
-    for row in cur.fetchall():
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    match = sys.argv[4]
+    myCursor = db.cursor()
+    myCursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
+                     ORDER BY states.id ASC".format(sys.argv[4]))
+    rows = myCursor.fetchall()
+    for row in rows:
         print(row)
-
-    cur.close()
+    myCursor.close()
     db.close()

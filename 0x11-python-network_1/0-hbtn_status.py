7c07deea2fd1fbb78764ca9e47656026a3a9a3d0
Joyce245 <oluwaseyifunmmyjoyce1@gmail.com>
2022-10-22 04:16:13 +0000

    New update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""
-import urllib.request
-
-
-def fetcher():
-    """fetcher"""
-    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode("utf-8")))
-
+"""Show the body of the header response"""
 if __name__ == "__main__":
-    fetcher()
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        print("Body response:")
+        ty = response.read()
+        print("\t- type: {}".format(type(response.read())))
+        print("\t- content: {}".format(ty))
+        print("\t- utf8 content: {}".format(ty.decode('utf-8')))
